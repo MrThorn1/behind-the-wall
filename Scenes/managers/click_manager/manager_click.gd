bb4131388e3dfc10a2c643a13fe7aa2f746f7444
Cole Thorn <heckatho@gmail.com>
2025-02-15 17:34:08 -0500

    Tutorial Complete

--- Scenes/managers/click_manager/manager_click.gd
+++ Scenes/managers/click_manager/manager_click.gd
@@ -2,6 +2,14 @@ class_name ManagerClick
 extends Node
 
 static var ref: ManagerClick
+var click_power : int = 0
+
+signal click_power_updated
+
+@onready var data : Data = Game.ref.data
+
+func _ready() -> void:
+	calculate_click_power()
 
 func _enter_tree() -> void:
 	if ref:
@@ -11,5 +19,14 @@ func _enter_tree() -> void:
 	ref = self
 
 func click() -> void:
-	ManagerGold.ref.create_gold(1)
+	ManagerGold.ref.create_gold(click_power)
 	pass
+
+func calculate_click_power() -> void:
+	var new_power : int = 1
+	new_power += 1 * data.click_power_01_level
+	new_power += 5 * data.click_power_02_level
+	new_power *= (1 + data.click_multiplier_01_level)
+	
+	click_power = new_power
+	click_power_updated.emit()
--- Scripts/data.gd
+++ Scripts/data.gd
@@ -1,3 +1,6 @@
 class_name Data
 
 var gold : int = 0
+var click_power_01_level : int = 12
+var click_power_02_level : int = 4
+var click_multiplier_01_level : int = 1
