fc726b6f512e521b2f884311a29ccabd29e3006a
Cole Thorn <heckatho@gmail.com>
2025-02-24 20:56:53 -0500

    20250224
    fixed building menu generation issue, can now dynamically build a full building menu from a resource group that is called from a specific folder in the project

--- Scenes/UI Scenes/build_menu.gd
+++ Scenes/UI Scenes/build_menu.gd
@@ -1,22 +1,25 @@
 class_name BuildMenu
 extends PanelContainer
 
-
+#pulls the packed scene for the individual ui instance we created that generates single slot for each building icon
 @export var slot_scene : PackedScene
+#references with a scene unique name the grid container in th ebottom of the vbox container
 @onready var grid_container: GridContainer = %GridContainer
+#new resource group pulling everything from the building resources folder
 var resource_group:ResourceGroup = load("res://Resources/Base Resource & Groups/building_resource_group.tres")
-var _content = [resource_group.load_all()]
+#loading all of those resources, might have to autoload in the future
+var _content = resource_group.load_all()
 
 func _ready():
 	hide()
 
 func open():
 	show()
+	#loops through all of the buildings in the new resource array, and adds them to the new grid container slot for each one
 	for building in _content:
 		var slot = slot_scene.instantiate()
 		grid_container.add_child(slot)
-		slot.display_building(building)
-	
+		slot.display(building)
 
 func _on_close_button_pressed() -> void:
 	hide()
