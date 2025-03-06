66485eac85337ff022d00101cc87c2d0c87121f0
Cole Thorn <heckatho@gmail.com>
2025-03-06 17:33:19 -0500

    20250306
    fixed building tie to mouse issue, was accidently setting the invisible building variable twice and wiping out the node 2d class.  build menu works correctly now, might still be a bug with building name passing putting a 2 in it for some reason.

--- Scripts/Split Screen/mouse_colony_root.gd
+++ Scripts/Split Screen/mouse_colony_root.gd
@@ -1,22 +1,32 @@
-extends Node
+extends Node2D
 
 var building:Building
 @export var building_placement_holder:Building
 var passed_name
+var building_selected : bool = false
 var resource_group:ResourceGroup = load("res://Resources/Base Resource & Groups/building_resource_group.tres")
 #loading all of those resources, might have to autoload in the future
 var _content = resource_group.load_all()
+var invisible_building : Node2D
 
 func _ready() -> void:
 	get_viewport().size = Vector2(886,1066)
 	pass # Replace with function body.
 
 func _on_build_menu_passed_to_colony(passed_name) -> void:
-
 	for building in _content:
 		if building.name == passed_name:
 			building_placement_holder = building
-			var invisible_building = building_placement_holder.scene.instantiate()
+			invisible_building = building_placement_holder.scene.instantiate()
 			add_child(invisible_building)
-			invisible_building.position = get_viewport().get_local_mouse_position()
+			#invisible_building.position = vector2(100,200)
+			building_selected = true
+			print(building_selected)
+			print(invisible_building)
 	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	if building_selected:
+		invisible_building.position = get_local_mouse_position()
+		pass
+	pass
