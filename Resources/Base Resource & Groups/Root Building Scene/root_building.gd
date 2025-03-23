88e13a0e6832f35b39bb9bc6228b00336df6d5df
Cole Thorn <heckatho@gmail.com>
2025-03-23 12:08:24 -0400

    20250323
    100% working builidng placement for single placement zone, should just be able to add the new colliders to the root building node, and setup the "can't build here" script in the placement loop, as well as setup the signals for each zone to tell the building placement poriton of the colony script where to place the building, and to position the building snap to the right spot, note: still need to fix, the trying to place too low bug where the mouse leaves the area and doesn't unsnap

--- Resources/Base Resource & Groups/Root Building Scene/root_building.gd
+++ Resources/Base Resource & Groups/Root Building Scene/root_building.gd
@@ -13,14 +13,10 @@ var cloth_cost
 var production_type
 var production_quantity
 var production_time
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if production_time:
-		var timer : Timer = Timer.new()
-		add_child(timer)
-		timer.start(production_time)
-		timer.timeout.connect(_timer_timeout)
+var sprite_size
+var sprite_size_x
+var sprite_size_y
+var sprite_holder
 
 func tweening() -> void:
 	var tween : Tween = create_tween()
@@ -30,3 +26,22 @@ func tweening() -> void:
 func _timer_timeout() -> void:
 	managerclick.ref.click(production_type,production_quantity)
 	tweening()
+	
+func reset_size_and_texture() -> void:
+	pass
+	
+func after_placed_population() -> void:
+	if production_time:
+		var timer : Timer = Timer.new()
+		add_child(timer)
+		timer.start(production_time)
+		timer.timeout.connect(_timer_timeout)
+	sprite_holder = get_child(0)
+	sprite_size = sprite_holder.texture.get_size()
+	sprite_holder.offset = Vector2(0, -(sprite_size.y/2))
+	pass
+	
+func after_invisible_population() -> void:
+	sprite_holder = get_child(0)
+	sprite_size = sprite_holder.texture.get_size()
+	sprite_holder.offset = Vector2(0, -(sprite_size.y/2))
--- Scripts/Split Screen/mouse_colony_root.gd
+++ Scripts/Split Screen/mouse_colony_root.gd
@@ -1,42 +1,50 @@
 extends Node2D
 
+var resource_group:ResourceGroup = load("res://Resources/Base Resource & Groups/building_resource_group.tres")
+var _content = resource_group.load_all()
+#loading all of those resources, might have to autoload in the future
+
+
 var building:Building
 var building_placement_holder
+var building_placement_holder_2
 var second_passed_name
-var building_selected : bool = false
-var building_in_build_area : bool = false
 var build_area_bottom_surface
-var resource_group:ResourceGroup = load("res://Resources/Base Resource & Groups/building_resource_group.tres")
-#loading all of those resources, might have to autoload in the future
-var _content = resource_group.load_all()
-var invisible_building : Node2D
-var placed_building : Node2D
 var platform_size
 var platform_position
+var temp_name
+var building_area_hovered_within
+
+var building_selected : bool = false
+var building_in_build_area : bool = false
+
+var invisible_building : Node2D
+var placed_building : Node2D
 var tweenscalechange : float = 1.5
 var pivot_offset : float
 var timer_wait_start : Timer = Timer.new()
 var time_mod : float = randf_range(.25, .5)
 var production_update_time : int
-var temp_name
-var building_area_hovered_within
 
 func _ready() -> void:
 	get_viewport().size = Vector2(886,1066)
 	build_area_bottom_surface = $platform3.position.y
+	#will need to be replaced with a signal input from the new collider function to pull the correct platform position upon entering
 	pass # Replace with function body.
 
 func _on_build_menu_passed_to_colony(passed_name) -> void:
 	for building in _content:
 		if building.building_name == passed_name:
-			building_placement_holder = load("res://Resources/Base Resource & Groups/Root Building Scene/root_building.tscn")
+			building_placement_holder = load("res://Resources/Base Resource & Groups/Root Building Scene/root_building.tscn").duplicate()
 			invisible_building = building_placement_holder.instantiate()
 			add_child(invisible_building)
-			populate_invisible_building(building, invisible_building)
+			invisible_building.name = "Temp_Invisible_Building"
+			populate_invisible_building(building)
+			$Temp_Invisible_Building.after_invisible_population()
 			#invisible_building.connect()
 			invisible_building.modulate.a = 0.5
 			building_selected = true
-			temp_name = invisible_building.name
+			temp_name = building.building_name
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
@@ -52,28 +60,16 @@ func _process(delta: float) -> void:
 			invisible_building.position.y = get_local_mouse_position().y
 			pass
 		if Input.is_action_just_pressed("Build_Building") and building_selected:
-			print(invisible_building.name)
+			invisible_building.queue_free()
 			for building in _content:
-				if building.building_name == invisible_building.name:
-					building_placement_holder = load("res://Resources/Base Resource & Groups/Root Building Scene/root_building.tscn")
-					placed_building = building_placement_holder.instantiate()
+				if building.building_name == temp_name:
+					building_placement_holder_2 = load("res://Resources/Base Resource & Groups/Root Building Scene/root_building.tscn").duplicate()
+					placed_building = building_placement_holder_2.instantiate()
 					add_child(placed_building)
-					populate_placed_building(building, placed_building)
-					#placed_building.trash_cost = building_placement_holder.trash_cost
-					#placed_building.food_cost = building_placement_holder.food_cost
-					#placed_building.wood_cost = building_placement_holder.wood_cost
-					#placed_building.metal_cost = building_placement_holder.metal_cost
-					#placed_building.elec_cost = building_placement_holder.elec_cost
-					#placed_building.fuel_cost = building_placement_holder.fuel_cost
-					#placed_building.cloth_cost = building_placement_holder.cloth_cost
-					#placed_building.production_type = building_placement_holder.production_type
-					#placed_building.production_quantity = building_placement_holder.production_quantity
-					#placed_building.production_time = building_placement_holder.production_time
+					placed_building.name = "Temp_Placed_Building"
+					populate_placed_building(building)
 					placed_building.position.x = invisible_building.position.x
 					placed_building.position.y = invisible_building.position.y
-					invisible_building.queue_free()
-					placed_building.name = temp_name
-					temp_name = invisible_building.name
 					building_selected = false
 					building_in_build_area = false
 	elif building_selected:
@@ -84,7 +80,6 @@ func _process(delta: float) -> void:
 func _on_platform_3_area_entered(area: Area2D) -> void:
 	if building_selected:
 		building_area_hovered_within = area
-		print(area)
 		invisible_building.modulate.g = 200
 		building_in_build_area = true
 	pass # Replace with function body.
@@ -95,26 +90,27 @@ func _on_platform_3_area_exited(area: Area2D) -> void:
 		building_in_build_area = false
 	pass # Replace with function body.
 	
-func populate_placed_building(building_placement_holder,placed_building) -> void:
-	$RootBuilding/Sprite2D.texture = building.building_sprite
-	#placed_building.name = building_placement_holder.name
+func populate_placed_building(building) -> void:
+	$Temp_Placed_Building/Sprite2D.texture = building.building_sprite
 	#placed_building.Sprite2D = building_placement_holder.building_sprite
-	##placed_building.AnimatedSprite2D = building_placement_holder.building_sprite_sheet
-	#placed_building.trash_cost = building_placement_holder.trash_cost
-	#placed_building.food_cost = building_placement_holder.food_cost
-	#placed_building.wood_cost = building_placement_holder.wood_cost
-	#placed_building.metal_cost = building_placement_holder.metal_cost
-	#placed_building.elec_cost = building_placement_holder.elec_cost
-	#placed_building.fuel_cost = building_placement_holder.fuel_cost
-	#placed_building.cloth_cost = building_placement_holder.cloth_cost
-	#placed_building.production_type = building_placement_holder.production_type
-	#placed_building.production_quantity = building_placement_holder.production_quantity
-	#placed_building.production_time = building_placement_holder.production_time
-	$RootBuilding.name = building.building_name
+	$Temp_Placed_Building.trash_cost = building.trash_cost
+	$Temp_Placed_Building.food_cost = building.food_cost
+	$Temp_Placed_Building.wood_cost = building.wood_cost
+	$Temp_Placed_Building.metal_cost = building.metal_cost
+	$Temp_Placed_Building.elec_cost = building.elec_cost
+	$Temp_Placed_Building.fuel_cost = building.fuel_cost
+	$Temp_Placed_Building.cloth_cost = building.cloth_cost
+	$Temp_Placed_Building.production_type = building.production_type
+	$Temp_Placed_Building.production_quantity = building.production_quantity
+	$Temp_Placed_Building.production_time = building.production_time
+	$Temp_Placed_Building.after_placed_population()
+	$Temp_Placed_Building.name = building.building_name
+	#placed_building.AnimatedSprite2D = building_placement_holder.building_sprite_sheet
 	pass
 	
-func populate_invisible_building(building, invisible_building) -> void:
-	$RootBuilding/Sprite2D.texture = building.building_sprite
+func populate_invisible_building(building) -> void:
+	$Temp_Invisible_Building/Sprite2D.texture = building.building_sprite
 	#$RootBuilding.name = building.building_name
 	#will need to instantiate and populate the buildingable collider here so that you can automatically create the buidling collider seperate from the buidlable area signal collider to generaete the size of the building to prepeve overwritting
+	pass
 pass
