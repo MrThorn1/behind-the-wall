d9420b3ca1588fd2b3a92f7dae2627706062b3d5
Cole Thorn <heckatho@gmail.com>
2025-06-29 10:53:38 -0400

    20250629_2
    FIxed the UI passing functions going down, and setting the viewports to continue to function outside of the actual individaul split screen scene nodes

--- Scenes/Resource_Drop/resource_drop.gd
+++ Scenes/Resource_Drop/resource_drop.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal resource_drop_selected
+var selected_for_raid : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,22 +13,32 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_area_2d_mouse_entered() -> void:
-	print("area entered worked")
 	if CurrencySingleton.raid_menu_open:
 		highlight_object()
 		pass
 	pass # Replace with function body.
 	
 func _on_area_2d_mouse_exited() -> void:
-	print("area excited worked")
 	unhighlight_object()
 	pass # Replace with function body.
 	
 func highlight_object() -> void:
-	$Sprite2D.modulate.g = 500
-	print("highlight activated")
+	$Sprite2D.modulate.g = 100
 	pass
 
 func unhighlight_object() -> void:
-	$Sprite2D.modulate.g = 1
-	print("highlight deactiveate")
+	if not selected_for_raid:
+		$Sprite2D.modulate.g = 1
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if CurrencySingleton.raid_menu_open:
+		print("raid menu open check passed")
+		if event is InputEventMouseButton:
+			print("mouse button event check passed")
+			if event.button_index ==1:
+				print("mouse button event passed correctly")
+				if selected_for_raid:
+					pass
+				selected_for_raid = true
+				emit_signal("resource_dropt_selected", self.name)
+	pass # Replace with function body.
